779675d54eb28efe575e81afe4540f5058c940ac
i432855 <432855@student.fontys.nl>
2020-09-29 14:38:00 +0200

    Basis functionaliteiten
    De basis staat nu sterk. Er is alleen nog weinig content. Video's staan niet op Github i.v.m. bestandgrootte

--- gamification.js
+++ gamification.js
@@ -1,4 +1,46 @@
+var button1 = document.getElementById("button1")
+var button2 = document.getElementById("button2")
+var film = document.getElementById("film")
+
 function vidend() {
-    document.getElementById("button1").style.visibility = "visible";
-    document.getElementById("button2").style.visibility = "visible";
+    button1.style.visibility = "visible";
+    button2.style.visibility = "visible";
+}
+
+function vid1() {
+    film.src = "vid2.mp4";
+    button1.style.visibility = "hidden";
+    button2.style.visibility = "hidden";
+    button1.setAttribute("onClick", "vid3()");
+    button2.setAttribute("onClick", "vid4()");
+}
+
+function vid2() {
+    film.src = "vid1.mp4";
+    button1.style.visibility = "hidden";
+    button2.style.visibility = "hidden";
+    alert("Wrong! Try again");
+}
+
+function vid3() {
+    film.src = "vid1.mp4";
+    button1.style.visibility = "hidden";
+    button2.style.visibility = "hidden";
+    button1.setAttribute("onClick", "vid1()");
+    button2.setAttribute("onClick", "vid2()");
+    alert("Wrong! Try again");
+}
+
+function vid4() {
+    film.src = "vid3.mp4";
+    button1.style.visibility = "hidden";
+    button2.style.visibility = "hidden";
+    button1.setAttribute("onClick", "vid5()");
+    button2.setAttribute("onClick", "vid5()");
+    button1.innerHTML = "Reset";
+    button2.innerHTML = "Reset";
+}
+
+function vid5() {
+    location.reload();
 }
